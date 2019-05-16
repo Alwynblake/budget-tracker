29a8c338d4c4af3476eb9bbb26abfebe8ad73f34
Alistair Blake <consultations@me.com>
2019-05-16 00:01:53 -0700

    handleChange and handleSubmit in form complete

--- src/reducer/section-reducer.js
+++ src/reducer/section-reducer.js
@@ -4,13 +4,28 @@ export default (state = [], {type, payload}) => {
     case'SECTION CREATE':
       return [...state, payload];
     case'SECTION UPDATE':
-      // *we are going to use map
+      // *we are going to use map *
+      // possible structure:
+//         handleUpdateSection = (section) =>
+//       this.setState((previousState) => {
+//   const updateSections = previousState.payload.map(currentSection =>
+//       section.id === currentSection.id ? section : currentSection
+//   );
+//   return {section: updateSections};
+// });
+
       return null;
     case'SECTION DELETE':
-      // *we are going to use filter
+      // *we are going to use filter *
+        // possible structure:
+        // handleRemoveSection = (section) => {
+        //   this.setState(previousState => ({
+        //     sections: previousState.sections.filter(currentSections => currentSections.id !== section.id),
+        //   }))
+        // };
+
       return null;
     default:
       return state;
-
   }
-}
+};
